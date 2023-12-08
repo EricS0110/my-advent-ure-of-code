9deb96a4b1d221e0b48487328257e57602a49c32
erics <ericsutton6@gmail.com>
2023-12-07 22:37:35 -0600

    Initial attempt at 7.2, needs debugging

--- 2023/day-07/puzzle_solution.py
+++ 2023/day-07/puzzle_solution.py
@@ -16,22 +16,8 @@ from shared.file_handling import get_file_strings
 #   I don't think it'll happen, but raise an exception on a perfect tie
 # Individual card ranks (Ace high):
 #  A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, 2
-SORT_ORDER = '23456789TJQKA'
+SORT_ORDER = '23456789TJQKA'  # This is used for the solution to part 1
 JOKER_ORDER = 'J23456789TQKA'  # This is used for the solution to part 2
-
-# Create a mapping from character to its order
-char_order_map = {char: index for index, char in enumerate(SORT_ORDER)}
-default_order = len(SORT_ORDER)  # Default order for characters not in custom char list
-joker_order_map = {char: index for index, char in enumerate(JOKER_ORDER)}
-joker_default = len(JOKER_ORDER)  # Default order for characters not in custom char list
-
-
-# Custom sorting function for strings
-def compare_keys(key1, key2):
-    for c1, c2 in zip(key1, key2):
-        if char_order_map.get(c1, default_order) != char_order_map.get(c2, default_order):
-            return char_order_map.get(c1, default_order) - char_order_map.get(c2, default_order)
-    return len(key1) - len(key2)  # If all characters are equal, shorter string comes first
 
 
 def parse_file(input_strings: list[str]) -> dict[str, dict[str, str]]:
@@ -60,11 +46,78 @@ def count_original_hands(game_dict: dict) -> dict[int, dict]:
 
 
 def count_joker_hands(game_dict: dict) -> dict[int, dict]:
+    for key, game_values in game_dict.items():
+        this_hand = game_values["original_hand"]
+        counts = Counter(this_hand)
+        all_matches = {count: [] for count in [1, 2, 3, 4, 5]}
+        for char, count in counts.items():
+            if count >= 1:
+                all_matches[count].append(char)
+        formatted_joker_matches = {k: v for k, v in all_matches.items() if v}
+        # At this point we've got a dict of {count: [chars], ...}
+        joker_count = 0
+        for count, char_list in formatted_joker_matches.items():
+            if "J" in char_list:
+                joker_count += count
+                char_list.remove("J")
+        game_dict[key]['joker_count'] = joker_count
 
+        game_dict[key]['counts'] = formatted_joker_matches
     return game_dict
 
 
-def group_results(game_dict: dict) -> dict[str, dict]:
+def label_hand(counts, count_by_keys, current_hand, use_jokers=False):
+    if use_jokers:
+        game_details = next(iter(current_hand.values()))
+        joker_count = game_details['joker_count']
+        if joker_count > 0:  # we now only need to worry about the case of having at least 1 Joker card
+            match joker_count:
+                case 5: return "five-of-a-kind"
+                case 4: return "four-of-a-kind"
+                case 3:
+                    if 2 in count_by_keys:
+                        return "five-of-a-kind"
+                    else:
+                        return "four-of-a-kind"
+                case 2:
+                    if 3 in count_by_keys:
+                        return "five-of-a-kind"
+                    elif 2 in count_by_keys:
+                        return "four-of-a-kind"
+                    else:
+                        return "three-of-a-kind"
+                case 1:
+                    if 4 in count_by_keys:
+                        return "five-of-a-kind"
+                    elif 3 in count_by_keys:
+                        return "four-of-a-kind"
+                    elif 2 in count_by_keys:
+                        return "three-of-a-kind"
+                    else:
+                        return "one-pair"
+
+    # Common logic for both cases (use_jokers True or False)
+    if len(counts) == 1:
+        if 5 in count_by_keys:
+            return "five-of-a-kind"
+        elif 4 in count_by_keys:
+            return "four-of-a-kind"
+        elif 3 in count_by_keys:
+            return "three-of-a-kind"
+        elif 2 in count_by_keys:
+            # Check the number of pairs for two-pair and one-pair
+            pair_count = len(counts[2])
+            if pair_count > 1:
+                return "two-pair"
+            elif pair_count == 1:
+                return "one-pair"
+    elif len(counts) == 2 and 2 in count_by_keys and 3 in count_by_keys:
+        return "full-house"
+
+    return "high-card"
+
+
+def group_results(game_dict: dict, use_jokers=False) -> dict[str, dict]:
     grouped_games = {}
     five_of_a_kind = {}
     four_of_a_kind = {}
@@ -76,33 +129,24 @@ def group_results(game_dict: dict) -> dict[str, dict]:
     for key, game in game_dict.items():
         this_hand = {key: game}
         count_keys = game['counts'].keys()
-
-        # Five-of-a-kind condition: len(counts) == 1 and 5 in count_keys
-        if len(game['counts']) == 1 and (5 in count_keys):
-            five_of_a_kind[key] = this_hand
-
-        # Four-of-a-kind condition: len(counts) == 1 and 4 in count_keys
-        elif (len(game['counts']) == 1) and (4 in count_keys):
-            four_of_a_kind[key] = this_hand
-
-        # Full House condition: len(counts) == 2 and 2, 3 both on count_keys
-        elif (len(game['counts']) == 2) and (2 in count_keys) and (3 in count_keys):
-            full_house[key] = this_hand
-
-        # Three-of-a-kind condition: len(counts) == 1 and 3 in count_keys
-        elif (len(game['counts']) == 1) and (3 in count_keys):
-            three_of_a_kind[key] = this_hand
-
-        # Two-pair conditions: len(counts) == 1 and 2 in count_keys
-        elif (len(game['counts']) == 1) and (2 in count_keys) and (len(game['counts'][2]) > 1):
-            two_pair[key] = this_hand
-
-        # One-pair conditions: len(counts) == 1 and 2 in count_keys and len(counts[2]) == 1
-        elif (len(game['counts']) == 1) and (2 in count_keys) and (len(game['counts'][2]) == 1):
-            one_pair[key] = this_hand
-
-        else:
-            high_card[key] = this_hand
+        hand_label = label_hand(game['counts'], count_keys, this_hand, use_jokers=use_jokers)
+        match hand_label:
+            case "five-of-a-kind":
+                five_of_a_kind[key] = this_hand
+            case "four-of-a-kind":
+                four_of_a_kind[key] = this_hand
+            case "full-house":
+                full_house[key] = this_hand
+            case "three-of-a-kind":
+                three_of_a_kind[key] = this_hand
+            case "two-pair":
+                two_pair[key] = this_hand
+            case "one-pair":
+                one_pair[key] = this_hand
+            case "high-card":
+                high_card[key] = this_hand
+            case _:
+                print("UNKNOWN RESULT RETURNED, DEBUG THIS")
 
         grouped_games['high_card'] = high_card
         grouped_games['one_pair'] = one_pair
@@ -115,13 +159,22 @@ def group_results(game_dict: dict) -> dict[str, dict]:
     return grouped_games
 
 
-def rank_games(grouped_games: dict) -> dict:
+def rank_games(grouped_games: dict, use_jokers=False) -> dict:
+    # Create a mapping from character to its order
+    char_order_map = {char: index for index, char in enumerate(SORT_ORDER)}
+    default_order = len(SORT_ORDER)  # Default order for characters not in custom char list
+    joker_order_map = {char: index for index, char in enumerate(JOKER_ORDER)}
+    joker_default = len(JOKER_ORDER)  # Default order for characters not in custom char list
+
     ranked_games = {}
     rank = 1
     for key, group in grouped_games.items():
         if len(group.values()) > 1:
             # Sort the keys of your_dict by custom character order
-            sorted_keys = sorted(group.keys(), key=lambda k: [char_order_map.get(c, default_order) for c in k])
+            if use_jokers:
+                sorted_keys = sorted(group.keys(), key=lambda k: [joker_order_map.get(c, joker_default) for c in k])
+            else:
+                sorted_keys = sorted(group.keys(), key=lambda k: [char_order_map.get(c, default_order) for c in k])
 
             # Create an OrderedDict with sorted keys
             ordered_dict = {}
@@ -160,6 +213,7 @@ if __name__ == '__main__':
     games_by_rank = rank_games(game_groups)
 
     # Part 1 solution
+    #   A result of 250254244 is correct
     game_winnings = calculate_game_winnings(games_by_rank)
     print(game_winnings)
 
@@ -169,3 +223,12 @@ if __name__ == '__main__':
     #       Need to tweak group_results logic to account for Joker support
     #       Need to tweak rank_games logic to allow for a configurable sort order
     joker_counted_hands = count_joker_hands(initial_data)
+    joker_game_groups = group_results(joker_counted_hands, use_jokers=True)
+    joker_games_by_rank = rank_games(joker_game_groups, use_jokers=True)
+
+    # Part 2 solution
+    #   A result of 252956851 is too high, need to debug further -> forgot to rework ranking to use Jokers
+    #   A result of 252663288 is still too high, need to debug further -> ?????
+    joker_game_winnings = calculate_game_winnings(joker_games_by_rank)
+    print(joker_game_winnings)
+
